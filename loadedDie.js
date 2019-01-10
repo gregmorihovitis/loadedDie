40a8826aac57a0da7850859395e3a0172a24a3f6
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-10 16:32:20 +0000

    completeed the loaded die assignment

---
+++ loadedDie.js
@@ -0,0 +1,15 @@
+function makeLoadedDie() {
+  const list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  let index = -1;
+
+  return function() {
+    index ++;
+    return list[index];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
